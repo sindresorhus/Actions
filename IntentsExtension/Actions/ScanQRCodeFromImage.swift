ce57c2db590109ccac7c48bb5515909d4cadd00c
Yasuaki Uechi <y@uechi.io>
2022-03-10 20:02:55 +0900

    Return nil when no QR code is found

--- IntentsExtension/Actions/ScanQRCodeFromImage.swift
+++ IntentsExtension/Actions/ScanQRCodeFromImage.swift
@@ -4,23 +4,26 @@ import Intents
 
 @MainActor
 final class ScanQRCodeFromImageIntentHandler: NSObject, ScanQRCodeFromImageIntentHandling {
-  func handle(intent: ScanQRCodeFromImageIntent) async -> ScanQRCodeFromImageIntentResponse {
-    let response = ScanQRCodeFromImageIntentResponse(code: .success, userActivity: nil)
+	func handle(intent: ScanQRCodeFromImageIntent) async -> ScanQRCodeFromImageIntentResponse {
+		guard let image = intent.image?.data else {
+			return .failure(failure: "Failed to obtain an image input.")
+		}
 
-    guard let image = intent.image?.data else {
-      return .failure(failure: "Failed to obtain an image input.")
-    }
+		guard let detector = CIDetector(ofType: CIDetectorTypeQRCode,
+		                                context: nil,
+		                                options: [CIDetectorAccuracy: CIDetectorAccuracyHigh]),
+			let ciImage = CIImage(data: image) else { return .failure(failure: "Failed to initialize QR Code scanner.") }
 
-    guard let detector = CIDetector(ofType: CIDetectorTypeQRCode,
-                                    context: nil,
-                                    options: [CIDetectorAccuracy: CIDetectorAccuracyHigh]),
-          let ciImage = CIImage(data: image) else { return .failure(failure: "Failed to initialize QR Code scanner.") }
+		let response = ScanQRCodeFromImageIntentResponse(code: .success, userActivity: nil)
 
-    guard let features = detector.features(in: ciImage) as? [CIQRCodeFeature],
-          let message = features.first?.messageString else { return .failure(failure: "No QR Code found in the image.") }
+		guard let features = detector.features(in: ciImage) as? [CIQRCodeFeature],
+		      let message = features.first?.messageString
+		else {
+			response.result = nil
+			return response
+		}
 
-    response.result = message
-
-    return response
-  }
+		response.result = message
+		return response
+	}
 }
