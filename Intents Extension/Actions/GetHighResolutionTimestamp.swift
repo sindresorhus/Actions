1113a52cbbb785e0807c2a04ceae307469698cbe
Sindre Sorhus <sindresorhus@gmail.com>
2023-09-27 18:10:50 +0700

    2.9.0

--- Intents Extension/Actions/GetHighResolutionTimestamp.swift
+++ Intents Extension/Actions/GetHighResolutionTimestamp.swift
@@ -9,7 +9,7 @@ Returns a timestamp representing the current instant in nanoseconds.
 
 Example: 434055845120916
 
-The most common use-case is to substract two instances of this to get a highly accurate difference.
+The most common use-case is to subtract two instances of this to get a highly accurate difference.
 
 The timestamp is not meant to be stored for a long time. It's only unique for the current computer session.
 """,
